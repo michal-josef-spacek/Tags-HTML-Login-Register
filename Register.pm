0f2359a1cb73ee6ded197cd3c5fa8f69b2617f74
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-29 12:02:50 +0200

    Finish example

--- Register.pm
+++ Register.pm
@@ -326,6 +326,8 @@ Returns undef.
 
 =head1 EXAMPLE
 
+=for comment filename=print_block_html_and_css.pl
+
  use strict;
  use warnings;
 
@@ -352,7 +354,85 @@ Returns undef.
  print $tags->flush."\n";
 
  # Output:
- # TODO
+ # CSS
+ # .form-register {
+ # 	width: ;
+ # 	background-color: #f2f2f2;
+ # 	padding: 20px;
+ # 	border-radius: 5px;
+ # 	box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+ # }
+ # .form-register fieldset {
+ # 	border: none;
+ # 	padding: 0;
+ # 	margin-bottom: 20px;
+ # }
+ # .form-register legend {
+ # 	font-weight: bold;
+ # 	margin-bottom: 10px;
+ # }
+ # .form-register p {
+ # 	margin: 0;
+ # 	padding: 10px 0;
+ # }
+ # .form-register label {
+ # 	display: block;
+ # 	font-weight: bold;
+ # 	margin-bottom: 5px;
+ # }
+ # .form-register input[type="text"], .form-register input[type="password"] {
+ # 	width: 100%;
+ # 	padding: 8px;
+ # 	border: 1px solid #ccc;
+ # 	border-radius: 3px;
+ # }
+ # .form-register button[type="submit"] {
+ # 	width: 100%;
+ # 	padding: 10px;
+ # 	background-color: #4CAF50;
+ # 	color: #fff;
+ # 	border: none;
+ # 	border-radius: 3px;
+ # 	cursor: pointer;
+ # }
+ # .form-register button[type="submit"]:hover {
+ # 	background-color: #45a049;
+ # }
+ # 
+ # HTML
+ # <form class="form-register" method="post">
+ #   <fieldset>
+ #     <legend>
+ #       Register
+ #     </legend>
+ #     <p>
+ #       <label for="username">
+ #       </label>
+ #       User name
+ #       <input type="text" name="username" id="username">
+ #       </input>
+ #     </p>
+ #     <p>
+ #       <label for="password1">
+ #         Password #1
+ #       </label>
+ #       <input type="password" name="password1" id="password1">
+ #       </input>
+ #     </p>
+ #     <p>
+ #       <label for="password2">
+ #         Password #2
+ #       </label>
+ #       <input type="password" name="password2" id="password2">
+ #       </input>
+ #     </p>
+ #     <p>
+ #       <button type="submit" name="register" value="register">
+ #         Register
+ #       </button>
+ #     </p>
+ #   </fieldset>
+ # </form>
 
 =head1 DEPENDENCIES
 
