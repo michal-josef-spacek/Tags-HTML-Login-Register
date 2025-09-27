7000c6c944b1724260e8472e4df1c1a3194ccaca
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-09-27 11:38:29 +0200

    Prepare for 0.10 version

--- Register.pm
+++ Register.pm
@@ -12,7 +12,7 @@ use Tags::HTML::Messages;
 
 Readonly::Array our @FORM_METHODS => qw(post get);
 
-our $VERSION = 0.09;
+our $VERSION = 0.10;
 
 # Constructor.
 sub new {
@@ -578,6 +578,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.09
+0.10
 
 =cut
